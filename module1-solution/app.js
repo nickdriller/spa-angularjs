a4592990cafabf79275f3c7d6b8701ed30fd5f83
Nicolas Silva <nisiara@gmail.com>
2021-03-04 23:57:42 -0300

    Add IIFE

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,19 +1,23 @@
-const app = angular.module('LunchCheck', [])
-app.controller('LunchController', function($scope){
-  $scope.meals = ''
-  $scope.message = ''
-  
-  $scope.checkMeals = function(){
-    const mealsArray = $scope.meals.split(',')
-    const mealsFiltered = mealsArray.filter( meal => meal)
-    if(mealsFiltered.length === 0 ){
-      $scope.message = 'Please enter data first ✏️'
-    } 
-    else if (mealsFiltered.length <= 3) {
-      $scope.message = 'Enjoy 😋'
+(function () {
+  const app = angular.module('LunchCheck', [])
+  app.controller('LunchCheckController', function($scope){
+    $scope.meals = ''
+    $scope.message = ''
+    
+    $scope.checkMeals = function(){
+      const mealsArray = $scope.meals.split(',')
+      const mealsFiltered = mealsArray.filter( meal => meal)
+      if(mealsFiltered.length === 0 ){
+        $scope.message = 'Please enter data first ✏️'
+      } 
+      else if (mealsFiltered.length <= 3) {
+        $scope.message = 'Enjoy 😋'
+      }
+      else if (mealsFiltered.length > 3) {
+        $scope.message = 'Hohoho, welcome to the club 🐷'
+      }
     }
-    else if (mealsFiltered.length > 3) {
-      $scope.message = 'Hohoho, welcome to the club 🐷'
-    }
-  }
-})
+  })
+})()
+
+
