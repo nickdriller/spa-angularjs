fc0482a3cf6a5fa21d500d112d3c6af78ccf43c3
Nicolas Silva <nisiara@gmail.com>
2021-03-04 23:45:44 -0300

    First commit

---
+++ module1-solution/app.js
@@ -0,0 +1,19 @@
+const app = angular.module('LunchCheck', [])
+app.controller('LunchController', function($scope){
+  $scope.meals = ''
+  $scope.message = ''
+  
+  $scope.checkMeals = function(){
+    const mealsArray = $scope.meals.split(',')
+    const mealsFiltered = mealsArray.filter( meal => meal)
+    if(mealsFiltered.length === 0 ){
+      $scope.message = 'Please enter data first ✏️'
+    } 
+    else if (mealsFiltered.length <= 3) {
+      $scope.message = 'Enjoy 😋'
+    }
+    else if (mealsFiltered.length > 3) {
+      $scope.message = 'Hohoho, welcome to the club 🐷'
+    }
+  }
+})
